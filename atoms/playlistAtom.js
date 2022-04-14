f9907397530187fd0897c59dff5291d4efa261bb
rosanno <rosannomacabuhay@gmail.com>
2022-04-14 12:58:05 +0800

    initial commit

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from 'recoil';
+
+export const playlistState = atom({
+  key: 'playlistState',
+  default: null,
+});
+
+export const playlistIdState = atom({
+  key: 'playlistIdState',
+  default: '2F3uE35x8nGf88jLsX1Hvs',
+});
---
+++ atoms/songAtom.js
@@ -0,0 +1,8 @@
+import { atom, selectorFamily } from 'recoil';
+
+export const currentTrackIdState = atom({
+  key: 'currentTrackIdState', // unique ID (with respect to other atoms/selectors)
+  default: null, // default value (aka initial value)
+});
+
+export const isPlayingState = atom({ key: 'isPlayingState', default: false });
